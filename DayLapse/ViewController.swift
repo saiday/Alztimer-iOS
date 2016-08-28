47bb4ac407a96982415b0458eb4dd9fdd7c1892e
Saiday <feocms@gmail.com>
2016-08-29 03:05:06 +0800

    UIImagePicker with .camera source, overlapped overlay view

--- DayLapse/ViewController.swift
+++ DayLapse/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import PureLayout
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: Views
+    lazy var shotButton: UIButton = {
+        let shotButton = UIButton(type: .system)
+        shotButton.setTitle("Shot Shot Shot Shot", for: .normal)
+        return shotButton
+    }()
+    
+    lazy var imagePickerController: UIImagePickerController = {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .camera
+        
+        let overlayView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 320))
+        let blueColor = UIColor.init(red: 255, green: 255, blue: 0, alpha: 0.5)
+        overlayView.backgroundColor = blueColor
+        overlayView.layer.isOpaque = false
+        overlayView.isOpaque = false
+        
+        imagePickerController.showsCameraControls = false
+        imagePickerController.cameraOverlayView = overlayView
+        
+        return imagePickerController
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.yellow()
+        
+        setupSubviews()
+        initCustomViews()
+    }
+    
+    func setupSubviews() {
+        view.addSubview(self.shotButton)
+        self.shotButton.autoCenterInSuperview()
+        
+    }
+    
+    func initCustomViews() {
+        view.backgroundColor = UIColor.white()
+        
+        self.shotButton.addTarget(self, action: #selector(self.shotButtonTapped), for: .touchUpInside)
+    }
+    
+    func shotButtonTapped() {
+        self.imagePickerController.delegate = self
+        
+        self.present(self.imagePickerController, animated: true, completion: nil)
     }
     
 }
