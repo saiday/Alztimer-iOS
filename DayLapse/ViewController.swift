bd46ae368299ec22a023064f8922a63f36735cf6
Saiday <feocms@gmail.com>
2016-10-10 02:00:24 +0800

    dismiss image picker on photo taken

--- DayLapse/ViewController.swift
+++ DayLapse/ViewController.swift
@@ -59,30 +59,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         photosAuthorizedStatus { [unowned self] (authorized) in
             if authorized {
-                self.accessDayLapseAlbum(albumFound: { (album) in
-                    PHPhotoLibrary.shared().performChanges({
-                        let assetRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
-                        let placeholder = assetRequest.placeholderForCreatedAsset
-                        let photosAsset = PHAsset.fetchAssets(in: album, options: nil)
-                        let albumChangeRequest = PHAssetCollectionChangeRequest(for: album, assets: photosAsset)
-                        let assets: NSArray = [placeholder!]
-                        albumChangeRequest!.addAssets(assets)
-                        }, completionHandler: { (success, error) in
-                    })
-                })
+                self.savePhoto(image: image) { (error) in
+                    if let err = error {
+                        print(err)
+                    }
+                    
+                    picker.dismiss(animated: true, completion: nil)
+                }
             } else {
                 print("no permission mdfk")
-                // TODO: this alert is persist on screen, bad UX
+                // FIXME: this alert is persist on screen, bad UX
                 self.imagePickerController.dismiss(animated: true, completion: { [unowned self] in
                     let alert = UIAlertController(title: NSLocalizedString("We need your permission to accss photo library", comment: ""), message: "", preferredStyle: .alert)
                     self.present(alert, animated: true, completion: nil)
                 })
             }
         }
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print("cancled")
     }
     
     func photosAuthorizedStatus(completion: @escaping (Bool) -> Void) -> Void {
@@ -96,6 +88,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    func savePhoto(image: UIImage, completion: @escaping (Error?) -> Void) {
+        accessDayLapseAlbum(albumFound: { (album) in
+            PHPhotoLibrary.shared().performChanges({
+                let assetRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+                let placeholder = assetRequest.placeholderForCreatedAsset
+                let photosAsset = PHAsset.fetchAssets(in: album, options: nil)
+                let albumChangeRequest = PHAssetCollectionChangeRequest(for: album, assets: photosAsset)
+                let assets: NSArray = [placeholder!]
+                albumChangeRequest!.addAssets(assets)
+                }, completionHandler: { (success, error) in
+                    DispatchQueue.main.async {
+                        completion(error)
+                    }
+            })
+        })
+    }
+    
     func accessDayLapseAlbum(albumFound: @escaping (PHAssetCollection) -> Void) {
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format: "title = %@", CUSTOM_ALBUM_NAME)
@@ -104,7 +113,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let album = collection.firstObject {
             albumFound(album)
         } else {
-            //If not found - Then create a new album
+            // If not found, create new album
             var assetCollectionPlaceholder = PHObjectPlaceholder()
             PHPhotoLibrary.shared().performChanges({
                 let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: CUSTOM_ALBUM_NAME)
@@ -112,7 +121,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 }, completionHandler: { success, error in
                     if (success) {
                         let collectionFetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [assetCollectionPlaceholder.localIdentifier], options: nil)
-                        print(collectionFetchResult)
                         albumFound(collectionFetchResult.firstObject!)
                     }
             })
