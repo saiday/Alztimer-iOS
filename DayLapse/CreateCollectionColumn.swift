5b4c7089b23185931b2cd3e85f90899e9c190745
Saiday <feocms@gmail.com>
2016-10-23 21:17:45 +0800

    [WIP] StackView

---
+++ DayLapse/CreateCollectionColumn.swift
@@ -0,0 +1,51 @@
+//
+//  CreateCollectionColumn.swift
+//  DayLapse
+//
+//  Created by Saiday on 10/10/16.
+//  Copyright © 2016 saiday. All rights reserved.
+//
+
+import UIKit
+
+import PureLayout
+
+protocol CreateCollectionColumnDelegate: class {
+    func createColllectionDidTapped()
+}
+
+class CreateCollectionColumn: UIView {
+    public weak var delegate: CreateCollectionColumnDelegate?
+    
+    weak var button: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+        initCustomViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupSubviews() {
+        let button = UIButton(type: .system)
+        addSubview(button)
+        button.autoCenterInSuperview()
+        self.button = button
+    }
+    
+    func initCustomViews() {
+        backgroundColor = UIColor.darkGray
+        
+        button.setTitle(NSLocalizedString("Create new collection", comment: ""), for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    func buttonTapped() {
+        if let delegate = self.delegate {
+            delegate.createColllectionDidTapped()
+        }
+    }
+}
--- DayLapse/ViewController.swift
+++ DayLapse/ViewController.swift
@@ -12,14 +12,7 @@ import PureLayout
 
 import Photos
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    // MARK: Views
-    lazy var shotButton: UIButton = {
-        let shotButton = UIButton(type: .system)
-        shotButton.setTitle("Shot Shot Shot Shot", for: .normal)
-        return shotButton
-    }()
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CreateCollectionColumnDelegate {
     lazy var imagePickerController: UIImagePickerController = {
         let imagePickerController = UIImagePickerController()
         imagePickerController.sourceType = .camera
@@ -30,6 +23,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         return imagePickerController
     }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(forAutoLayout: ())
+        stackView.alignment = .fill
+        // TODO: try try look
+//        stackView.axis = UILayoutConstraintAxisVertical;
+//        stackView.distribution = UIStackViewDistributionEqualSpacing;
+//        stackView.alignment = UIStackViewAlignmentCenter;
+//        stackView.spacing = 30;
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,17 +42,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func setupSubviews() {
-        view.addSubview(self.shotButton)
-        self.shotButton.autoCenterInSuperview()
+        view.addSubview(stackView)
+        stackView.autoCenterInSuperview()
+        stackView.autoMatch(.width, to: .width, of: view)
+        stackView.autoMatch(.height, to: .height, of: view)
     }
     
     func initCustomViews() {
         view.backgroundColor = UIColor.white
         
-        self.shotButton.addTarget(self, action: #selector(self.shotButtonTapped), for: .touchUpInside)
+        let createCollectionView = CreateCollectionColumn()
+        createCollectionView.delegate = self
+        
+        let view1 = UIView()
+        view1.backgroundColor = UIColor.blue
+        
+        stackView.addArrangedSubview(createCollectionView)
+        stackView.addArrangedSubview(view1)
     }
     
-    func shotButtonTapped() {
+    func createColllectionDidTapped() {
         self.imagePickerController.delegate = self
         self.present(self.imagePickerController, animated: true, completion: nil)
     }
